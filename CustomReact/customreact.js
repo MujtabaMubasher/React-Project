517b94fa4d81eedafa0382b26c0ced8967016f57
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2023-12-24 17:51:49 +0000

    Done

---
+++ CustomReact/customreact.js
@@ -0,0 +1,35 @@
+
+function customRendor(newElement , customDom){
+    // const domElement = document.createElement(newElement.type)
+    // domElement.setAttribute('href' , newElement.props.href)
+    // domElement.setAttribute('target' , newElement.props.target)
+    // domElement.innerHTML = newElement.childern
+
+    // customDom.appendChild(domElement)
+
+    /*__________________________________________________________________ */
+
+    const domElement = document.createElement(newElement.type)
+
+    for (const key in newElement.props) {
+          
+        domElement.setAttribute(key, newElement.props[key] )
+    }
+    domElement.innerHTML = newElement.childern
+    customDom.appendChild(domElement)
+}
+
+const newElement = {
+    type: 'a',
+    props: {
+        href : 'https://www.google.com',
+        target: '_blank',
+        style: ''
+
+    },
+    childern: 'Visit to Google'
+    
+}
+const customDom = document.getElementById('root') 
+
+customRendor(newElement , customDom)
