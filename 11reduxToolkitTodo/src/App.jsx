00f4bc232737ae9e29b0a6cb54cb9ee8a668e1d9
Mujtaba Mubasher <81616586+MujtabaMubasher@users.noreply.github.com>
2024-02-28 04:24:37 +0000

    Done

---
+++ 11reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+      <AddTodos />
+      <Todos />
+    </>
+  )
+}
+
+export default App
